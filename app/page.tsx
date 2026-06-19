819e7fbca1f0729786cb18bf4f9ebac529f83896
jayesh-0011 <jayesh.sulakshane@gmail.com>
2026-06-19 10:46:32 +0530

    fixed some unused variables

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
-import { createClient } from '@/utils/supabase/server'
-import { cookies } from 'next/headers'
-import conversion from '@/utils/import/conversion'
-import updateTable from '@/utils/import/updateTable'
 import UserInputForm from './components/userInput'
 
 export default async function Page() {
-  const cookieStore = await cookies()
-  const supabase = createClient(cookieStore)
   return (
     <>
-      <UserInputForm/>
+      <UserInputForm />
     </>
   )
 }
